a89630a13fa1053a88088f66d39a613a3a80da3f
IacovColisnicenco <jacobwebm@gmail.com>
2023-03-15 20:37:50 +0200

    ass serv.js

---
+++ js/serv.js
@@ -0,0 +1,219 @@
+/** @format */
+
+"use strict";
+
+// console.log('arr' + "- object");
+// console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+
+    // ++incr;
+    // --decr;
+
+    // console.log(incr++);
+    // console.log(decr--);
+
+
+    // console.log(incr);
+    // console.log(decr);
+
+    // console.log(5%2);
+
+    console.log(2+2*2 !=='6' );
+
+
+    // const isChecked = false,
+    //       isClose = false;
+    //       console.log(isChecked || !isClose);
+
+
+
+
+// const  category = 'toys';
+
+// console.log(`https://someurl/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+// const answers  = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фимилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer + 5);
+// const result = confirm("Are you here");
+// console.log(result);
+
+// alert('ddd');
+// const arr = ['a', 'b', 'c'];
+
+// arr[10] = '325554ddddd';
+// console.log(arr);
+
+
+// const arrObj = {
+//     a: 'a',
+//     '1': 'b',
+//     2: 'c',
+//     abc: {
+//         df: [{}, {}],
+//         def: {
+
+//         }
+//     }
+// };
+
+// const b = {'b'};
+
+// arrObj[b] = '12345';
+// //console.log(arr[1]);
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// const arr = [1,2,3];
+// console.log(arr[1]);
+
+// const obj = {
+//     a:1,
+    
+//     b:2
+
+// };
+
+// const obj = {
+
+//     Anna: 500,
+//     'Alice': 800
+// };
+
+// ['Anna', 500, 'Alice', 800];
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+// let number = 4.6;
+// console.log(-4/0);
+// console.log('String' * 9);
+
+// const persone = `5`;
+
+// const bool = false;
+
+//console.log(something);
+
+// let und ;
+
+// console.log(und);
+
+// const obj = {
+
+//     name: "Johndd",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// if (1) {
+//     console.log('Ok!');
+// } else {
+
+//     console.log('Error');
+// }
+// const num = 515654654;
+
+// switch (num) {
+// 	case 49:
+// 		console.log("Incorrect");
+// 		break;
+// 	case 100:
+// 		console.log("Incorrect");
+// 		break;
+// 	case 51:
+// 		console.log("В точку!");
+// 		break;
+
+// 	default:
+//         console.log('fdlskjfsdgjkhfdsljkh');
+// 		break;
+// }
+
+//  (num === 50) ? console.log('Ok') : console.log('Error');
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Obbbk!');
+// }
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+// console.log("Ширина кузова автомобиля"+ a + ', длина:' + b);
+
+//['dhdhdh', 'ererer', 'dssssss'].map(a =>)
+
+//Request
+// data
+//response
+
+//snake_case
+//UPPER_SNAKE_CASE
+//Kebab_case
+// PascalCase
+
+//  const COLOR_RED = '#F00';
+
+//
+
+// let arr = [];
+// let a = arr[].push('a','b');
+// console.log(arr.a[0]);
+//  let a = 15
+//  console.log(a)
+
+// let number = 5;
+
+// const PI = 3.14; //используем const везде где возможно
+
+// number = 10;
+// //PI = 10;
+// console.log(number);
+
+// const obj = {
+
+//     a: 'ddddd'
+// };
+
+// obj.a= 'ddddddddddddddddddddddd5555555555555555555555555555555555555xx';
+
+// console.log(`The lenght of string is: ${obj.a.length}`);
+
+//var name = 'Ivan';
+
+// console.log(name);
+
+// var name = 'Alex';
+
+// // console.log(number);
+// // let number = 5;
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
